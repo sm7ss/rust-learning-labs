b38b648684e34c74c0e177844009ec6f581ed042
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-03-21 01:40:24 -0200

    Okay, this is more or less the structure of your student grading project. In this I would like to highlight the importance of the provision and 'packaging' of vairables and then use them and avoid the intense constant copying in real memory, slowing down the program and also the strictness of typing, I love that 🐥

--- BeginningLabs/functions_exercise/src/main.rs
+++ BeginningLabs/functions_exercise/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    let y: i8 = 8;
+    let grades: [i32; 10] = [85, 92, 78, 65, 95, 88, 72, 90, 68, 81];
+    
+    println!("=== Analyse Student Grades ==");
+    
+    let average: f64 = average_grade(&grades);
+    let max: i32 = max_grade(&grades);
+    
 }
+
+fn average_grade(grade: &[i32]) -> f64 {
+    let sum: i32 = grade.iter().sum(); 
+    let average: f64 = sum as f64 / grade.len() as f64;
+    average
+}
+
+fn max_grade(grade: &[i32]) -> i32 {
+    let max: i32 = *grade.iter().max().unwrap_or(&0);
+    max
+}
+
+//fn min_grade(grade: &[i32]) -> i32 {}
+
+//fn count_approved(grade: &[i32]) -> usize {}
+
+//fn count_failed(grade: &[i32]) -> usize {}
+
+//fn letter_note(grade: i32) -> char {}
+
+
+
