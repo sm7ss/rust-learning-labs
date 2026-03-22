948f367a98f20f617018f68a6801021c7ae238c0
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-03-22 02:46:18 -0200

    Update of the grading program, right now I am focusing on the validation of entries, just in case because I try to adjust it to the program to avoid possible errors such as, for example, you can change the passing grade and go from 70 to 60 for example, also validate the data type as the iteration of minimum maximum values in the same array

--- BeginningLabs/functions_exercise/src/main.rs
+++ BeginningLabs/functions_exercise/src/main.rs
@@ -1,10 +1,26 @@
+use std::any::type_name;
+
+const MAX_GRADE: i32 = 100; 
+const MIN_GRADE: i32 = 0;
+
+fn data_type<T>(_: &T) {
+    println!("{}", type_name::<T>());
+}
+
 fn main() {
     let grades: [i32; 10] = [85, 92, 78, 65, 95, 88, 72, 90, 68, 81];
+    
+    
     
     println!("=== Analyse Student Grades ==");
     
     let average: f64 = average_grade(&grades);
     let max: i32 = max_grade(&grades);
+    let min: i32 = min_grade(&grades);
+    let approved: usize = count_approved(&grades);
+    
+    
+    data_type(&average);
     
 }
 
@@ -19,9 +35,21 @@ fn max_grade(grade: &[i32]) -> i32 {
     max
 }
 
-//fn min_grade(grade: &[i32]) -> i32 {}
+fn min_grade(grade: &[i32]) -> i32 {
+    let min: i32 = *grade.iter().min().unwrap_or(&0);
+    min
+}
 
-//fn count_approved(grade: &[i32]) -> usize {}
+fn count_approved(grade: &[i32]) -> usize {
+    let mut count: usize= 0;
+    for &g in grade {
+        match g {
+            70..=100 => {count+=1}, 
+            _ => {continue}
+        }
+    }
+    count
+}
 
 //fn count_failed(grade: &[i32]) -> usize {}
 
