5f0f5d2f808b58cd65748809622e426e97b9f954
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-03-23 03:20:39 -0200

    The minimum and maximum ratings have now been validated, the validation structure has also been changed, the original list is mutated in case these values within the list are less or greater than those allowed and it also changes them, possibly enums are added

--- BeginningLabs/functions_exercise/src/main.rs
+++ BeginningLabs/functions_exercise/src/main.rs
@@ -1,40 +1,60 @@
-const MAX_GRADE: i32 = 100; 
-const MIN_GRADE: i32 = 0;
+const MAX_GRADE: u32 = 100; 
+const MIN_GRADE: u32 = 0;
+const VALUE_GRADE_IF_MIN_MAX: u32 = 0;
+
+fn max_min_grades_validation(grade: &mut[u32]) -> usize {
+    let mut changes: usize = 0;
+    
+    for g in grade {
+        if *g < MIN_GRADE {
+            *g = VALUE_GRADE_IF_MIN_MAX;
+            println!("Your grade value cannot be less than {} Your new grade: {}", MIN_GRADE, VALUE_GRADE_IF_MIN_MAX);
+            changes+=1;
+        } else if *g > MAX_GRADE {
+            *g = VALUE_GRADE_IF_MIN_MAX;
+            println!("Your grade value cannot be greater than {} Your new grade: {}", MAX_GRADE, VALUE_GRADE_IF_MIN_MAX);
+            changes+=1;
+        }
+    }; 
+    
+    changes
+}
 
 fn main() {
-    let grades: [i32; 10] = [85, 92, 78, 65, 95, 88, 72, 90, 68, 81];
+    let mut grades: [u32; 10] = [85, 92, 78, 65, 95, 110, 72, 90, 68, 81];
+    let change: usize = max_min_grades_validation(&mut grades);
     
-    
+    if change > 0 {
+        println!("New grades: {:?}", grades);
+    } else {
+        println!("All grades are fine");
+    }
     
     println!("=== Analyse Student Grades ==");
     
     let average: f64 = average_grade(&grades);
-    let max: i32 = max_grade(&grades);
-    let min: i32 = min_grade(&grades);
+    let max: u32 = max_grade(&grades);
+    let min: u32 = min_grade(&grades);
     let approved: usize = count_approved(&grades);
-    
-    
-    data_type(&average);
-    
 }
 
-fn average_grade(grade: &[i32]) -> f64 {
-    let sum: i32 = grade.iter().sum(); 
+fn average_grade(grade: &[u32]) -> f64 {
+    let sum: u32 = grade.iter().sum(); 
     let average: f64 = sum as f64 / grade.len() as f64;
     average
 }
 
-fn max_grade(grade: &[i32]) -> i32 {
-    let max: i32 = *grade.iter().max().unwrap_or(&0);
+fn max_grade(grade: &[u32]) -> u32 {
+    let max: u32 = *grade.iter().max().unwrap_or(&0);
     max
 }
 
-fn min_grade(grade: &[i32]) -> i32 {
-    let min: i32 = *grade.iter().min().unwrap_or(&0);
+fn min_grade(grade: &[u32]) -> u32 {
+    let min: u32 = *grade.iter().min().unwrap_or(&0);
     min
 }
 
-fn count_approved(grade: &[i32]) -> usize {
+fn count_approved(grade: &[u32]) -> usize {
     let mut count: usize= 0;
     for &g in grade {
         match g {
@@ -45,9 +65,9 @@ fn count_approved(grade: &[i32]) -> usize {
     count
 }
 
-//fn count_failed(grade: &[i32]) -> usize {}
+//fn count_failed(grade: &[u32]) -> usize {}
 
-//fn letter_note(grade: i32) -> char {}
+//fn letter_note(grade: u32) -> char {}
 
 
 
